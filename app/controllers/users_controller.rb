9a15eaafbac540e82613cc20fcbf410cffd824d6
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-10-19 04:11:32 +0900

    [fix]view- In the middle of development

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @post = user.posts
+    @posts = user.posts
     # @tweets = user.tweets.order("created_at DESC")
   end
 end
