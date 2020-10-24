2f2182ffe13c1b501adb39a6680835a4c6566144
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-10-25 03:47:37 +0900

    [add]view-mypage-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @nickname = user.nickname
     @posts = user.posts
+    @kaminari = Post.page(params[:page]).per(9)
     # @tweets = user.tweets.order("created_at DESC")
   end
 end
