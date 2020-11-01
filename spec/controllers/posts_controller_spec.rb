a6aa5a7f7555c26525f122f334f74eb4c6ed0a0a
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-11-01 18:31:22 +0900

    [add]rspec-postcontroller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-describe PostsController do
+describe PostsController, type: :controller do
 
+  describe 'GET #new' do
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+  
 end
