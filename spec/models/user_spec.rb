7531d1b67f0c17aa518bc196dcb121e884b56607
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-11-29 05:32:05 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,46 +3,54 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
+    # 1. nicknameとemail、passwordとpassword_confirmationが存在すれば登録できること
     it "is valid with a nickname, email, password, password_confirmation" do
       user = build(:user)
       expect(user).to be_valid
     end
 
+    # 2. nicknameが空では登録できないこと
     it "is invalid without a nickname" do
       user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
 
+    # 3. emailが空では登録できないこと
     it "is invalid without a email" do
       user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
+    # 4. passwordが空では登録できないこと
     it "is invalid without a password" do
       user = build(:user, password: "")
       user.valid?
       expect(user.errors[:password]).to include("can't be blank")
     end
 
+    # 5. passwordが存在してもpassword_confirmationが空では登録できないこと
     it "is invalid without a password_confirmation" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
 
+    # 6. nicknameが7文字以上であれば登録できないこと
     it "is invalid with a nickname that has more than 9 characters" do
       user = build(:user, nickname: "aaaaaaaaa")
       user.valid?
       expect(user.errors[:nickname]).to include("is too long (maximum is 8 characters)")
     end
 
+    # 7. nicknameが6文字以下では登録できること
     it "is valid with a nickname that has less than 8 characters" do
       user = build(:user, nickname: "aaaaaaaa")
       expect(user).to be_valid
     end
 
+    # 8. 重複したemailが存在する場合登録できないこと
     it "is invalid with a duplicate email address" do
       user = create(:user)
       another_user = build(:user, email: user.email)
@@ -50,11 +58,13 @@ describe User do
       expect(another_user.errors[:email]).to include("has already been taken")
     end
 
+    # 9. passwordが6文字以上であれば登録できること
     it "is valid with a password that has more than 6 characters" do
       user = build(:user, password: "000000", password_confirmation: "000000")
       expect(user).to be_valid
     end
 
+    # 10. passwordが5文字以下であれば登録できないこと
     it "is invalid without a password of 5 characters or less" do
       user = build(:user, password: "00000", password_confirmation: "00000")
       user.valid?
