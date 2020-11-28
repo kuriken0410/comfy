5ccc2be5cbf829c0dbf9ad26d68f8a88f0ed65e0
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-11-29 05:44:47 +0900

    [add]test_PostsController

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -8,6 +8,20 @@ describe PostsController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'GET #edit' do
+    it "assigns the requested post to @post" do
+      post = create(:post)
+      get :edit, params: { id: post }
+      expect(assigns(:post)).to eq post
+    end
+    it "renders the :edit template" do
+      post = create(:post)
+      get :edit, params: { id: post }
+      expect(response).to render_template :edit
+    end
+  end
+
   describe 'GET #index' do
     it "populates an array of posts ordered by created_at DESC" do
       posts = create_list(:post, 3)
