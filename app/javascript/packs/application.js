64e07c20823b555d0a2ef1ec87c45541eeec2a6c
kuriken0410 <67869934+kuriken0410@users.noreply.github.com>
2020-12-24 06:33:59 +0900

    [add]webpacker_jquery_bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require('comment')
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -16,7 +18,7 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import '@fortawesome/fontawesome-free/js/all'
-import '../css/application.scss'
+import '../src/application.scss'
 
-import 'bootstrap';
-import '../css/application';
+import 'bootstrap'
+import '../src/application.scss'
